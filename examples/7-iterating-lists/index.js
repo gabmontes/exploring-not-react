bf75cbad9d174e93dff766960ea9b623756c9466
Gabriel Montes <gabriel@utoctadel.com.ar>
2016-11-05 21:24:57 -0300

    Adds iteration example

---
+++ examples/7-iterating-lists/index.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5]
+
+const listItems = numbers.map(number => `
+  <li>${number}</li>
+`).join('')
+
+$(function () {
+  notReactDOM.render(
+    `<ul>${listItems}</ul>`,
+    document.getElementById('root')
+  )
+})
